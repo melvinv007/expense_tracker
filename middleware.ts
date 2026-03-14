b3f5e14d2ba49e3a0c1e9d199cea9dc91978994c
Melvin <vmelvin299@gmail.com>
2026-03-14 18:20:37 +0530

    add password protection

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const auth = req.cookies.get('auth')?.value;
+  if (auth === process.env.SITE_PASSWORD) return NextResponse.next();
+
+  const { pathname } = req.nextUrl;
+  if (pathname === '/login') return NextResponse.next();
+
+  return NextResponse.redirect(new URL('/login', req.url));
+}
+
+export const config = {
+  matcher: ['/((?!_next|favicon.ico).*)'],
+};
